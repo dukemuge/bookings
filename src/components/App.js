8521923cb3d1569d607fa3a79b41cd73c82cc49b
dukemuge <dukemuge232@gmail.com>
2021-10-11 16:37:53 +0300

    Add CSS styling  to style comonents

--- src/components/App.js
+++ src/components/App.js
@@ -7,3 +7,26 @@ import {
 } from "react-router-dom";
 import "../App.css";
 import { FaCalendarAlt, FaDoorOpen, FaUsers } from "react-icons/fa";
+import BookablesPage from "./Bookables/BookablesPage";
+import BookingsPage from "./Bookings/BookingsPage";
+import UsersPage from "./Users/UsersPage";
+import UserPicker from "./Users/UserPicker.js";
+
+export default function () {
+    return (
+        <Router>
+            <div className="App">
+                <header>
+
+                </header>
+
+                <Router>
+
+                </Router>
+
+
+            </div>
+        </Router>
+
+    );
+}
