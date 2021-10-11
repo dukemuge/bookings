024d5a497bb4c9285c696c28fb5325a6e5422ac2
dukemuge <dukemuge232@gmail.com>
2021-10-11 16:21:56 +0300

    Added Structured folder for various components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header classiName="App-header">
+        <p>
+          Hi, iM using  raect
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import { QueryClient, QueryClientProvider } from "react-query";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
+import "../App.css";
+import { FaCalendarAlt, FaDoorOpen, FaUsers } from "react-icons/fa";
