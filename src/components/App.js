da7c48b3d217ff8dd848c44a437ed91227a87524
dukemuge <dukemuge232@gmail.com>
2021-10-11 17:29:08 +0300

    Added react-router and Links to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import {
     Route,
     Link
 } from "react-router-dom";
+import React from 'react';
+
 import "../App.css";
 import { FaCalendarAlt, FaDoorOpen, FaUsers } from "react-icons/fa";
 import BookablesPage from "./Bookables/BookablesPage";
@@ -17,12 +19,34 @@ export default function () {
         <Router>
             <div className="App">
                 <header>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/bookings" className="btn   btn-header"></Link>
+                                <FaCalendarAlt />
+                                <span>Bookings</span>
+                            </li>
+                            <li>
+                                <Link to="/bookables" className="btn   btn-header"></Link>
+                                <FaDoorOpen />
+                                <span>Bookables</span>
+                            </li>
+                            <li>
+                                <Link to="/users" className="btn   btn-header"></Link>
+                                <FaUsers />
+                                <span>Users</span>
+                            </li>
+                        </ul>
+                    </nav>
 
                 </header>
+                <UserPicker />
 
-                <Router>
-
-                </Router>
+                <Routes>
+                    <Route path="/bookings" element={<BookingsPage />} />
+                    <Route path="/bookables" element={<BookablesPage />} />
+                    <Route path="/users" element={<UsersPage />} />
+                </Routes>
 
 
             </div>
